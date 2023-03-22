8e78901ff8859c17c6f0cedacbcb3ec7da3da687
Renjithkumar993 <121647888+Renjithkumar993@users.noreply.github.com>
2023-03-22 09:49:34 -0300

    adding array of questins

--- index.js
+++ index.js
@@ -10,34 +10,17 @@ const PressToContinuePrompt = require('inquirer-press-to-continue');
 
 inquirer.registerPrompt('press-to-continue', PressToContinuePrompt);
 
-async function callInquirers() {
 
-    const inq1 = await inquirer.prompt(
-        {
-            name: 'key',
-            type: 'press-to-continue',
-            enter: true,
-            pressToContinueMessage: `'Press Enter key to continue...'\n`,
-        });
 
-    const inq2 = await inquirer.prompt(
+const questions = {
+    sameQuestions :[
         {
-            type:"list",
-            name:"shape",
-            message: "Please specifiy the shape of your logo",
-            choices : ["circle","triangle","square"]
+        type:"list",
+        name:"shape",
+        message: "Please specifiy the shape of your logo",
+        choices : ["circle","triangle","square"]
         },
-    ).then(({shape})=>{
-        if (shape ==="circle"){
-            console.log("circle chosen"
-            )
-        }else if(shape === "triangle"){
-            console.log("trianbgle chosen")
-        }
-    })
-
-    const inq3 = await inquirer.prompt([{
-
+        { 
         type: "input",
         name: "text",
         message: "Enter the 3 digit text you would like to have on the Logo",
@@ -48,39 +31,70 @@ async function callInquirers() {
             return true;
         }
 
-    },
-    {
-        type:"input",
-        name:"color",
-        message: "Please specifiy the color or a hexadecimal number of the color you would like to have as text color",
-        validate: (input) => {
-            if (input.match([/^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/])) {
-                return "Ooops, looks like you have typo";
+        },
+        {
+            type:"input",
+            name:"color",
+            message: "Please specifiy the color or a hexadecimal number of the color you would like to have as text color",
+            validate: (input) => {
+                if (input.match([/^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/])) {
+                    return "Ooops, looks like you have typo";
+                }
+                return true;
             }
-            return true;
-        }
-    },
-    {
-        type:"input",
-        name:"shapeColor",
-        message: "Please specifiy the shape color or a hexadecimal number of the color you would like to have on the shape",
-        validate: (input) => {
-            if (input.match(/^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/)) {
-                return "Ooops, looks like you have typo";
+
+        },
+        {
+            type:"input",
+            name:"shapeColor",
+            message: "Please specifiy the shape color or a hexadecimal number of the color you would like to have on the shape",
+            validate: (input) => {
+                if (input.match(/^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/)) {
+                    return "Ooops, looks like you have typo";
+                }
+                return true;
             }
-            return true;
+
         }
-    },
-
-
-
-    ]).then(({text,color,shape,shapeColor}) =>{
-
-        console.log(text,color,shape,shapeColor)
         
-    })
+    ] 
+}
+
+
+
+
+
+
+
+
+async function callInquirers() {
+
+    const inq1 = await inquirer.prompt(
+        {
+            name: 'key',
+            type: 'press-to-continue',
+            enter: true,
+            pressToContinueMessage: `'Press Enter key to continue...'\n`,
+        });
 
     }
+    
+    const {shape} = await inquirer.prompt(questions.sameQuestions)
+    try {
+    switch (shape) {
+        case "circle":
+            await ;
+            break;
+    }
+} catch (err) {
+    console.error(err)
+}
 
+
+
+
+function circlePromt(){
+    ...questions.sameQuestions
+}
 
 callInquirers();
